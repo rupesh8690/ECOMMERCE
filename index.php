d33131790244f13d9daed1923e82e9d7208d0f42
rupesh8690 <rupeshlove8690@gmail.com>
2023-10-20 17:23:34 +0545

    added functionalities to index(dont show register when users are logged in)

--- index.php
+++ index.php
@@ -55,9 +55,26 @@ session_start();
         <a class="nav-link" href="display_all.php">Products</a>
       </li>
 
-      <li class="nav-item">
-        <a class="nav-link" href="./users_area/user_registration.php">Register</a>
-      </li>
+      <?php
+      if(!isset($_SESSION['username']))
+      {
+        echo " <li class='nav-item'>
+        <a class='nav-link' href='./users_area/user_registration.php'>Register</a>
+      </li>";
+      }
+      else
+      {
+       
+        echo " <li class='nav-item'>
+        <a class='nav-link' href='./users_area/profile.php'>My Account</a>
+      </li>";
+
+      }
+
+
+      ?>
+
+     
 
       <li class="nav-item">
         <a class="nav-link" href="#">Contact</a>
