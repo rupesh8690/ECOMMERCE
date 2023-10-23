2a4d2a15b81ae5e7f2f0954ff53543a2e21a6be9
rupesh8690 <rupeshlove8690@gmail.com>
2023-10-23 17:45:32 +0545

    added edit product features

--- admin/edit_products.php
+++ admin/edit_products.php
@@ -35,6 +35,116 @@ if(isset($_GET['edit_products']))
 
   
 }
+
+if(isset($_POST['edit_product']))
+{
+    $update_title=$_POST['product_title'];
+    $update_description=$_POST['product_description'];
+    $update_keyword=$_POST['product_keyword'];
+
+ 
+
+    //getting category
+    $update_category=$_POST['product_category'];
+
+        //getting brand
+        $update_brand=$_POST['product_brand'];
+
+    //getting category id on the basis of category title
+    $select_new_cat_id = "SELECT * FROM categories WHERE category_title='$update_category'";
+    $result_new_cat_id = mysqli_query($conn, $select_new_cat_id);
+    $row_new_cat_id = mysqli_fetch_assoc($result_new_cat_id);
+    
+    if ($row_new_cat_id) {
+        $new_cat_id = $row_new_cat_id['category_id']; // Use square brackets
+    } else {
+        // Handle the case where the category was not found.
+        echo "<script>alert('problem')</script>";
+    }
+
+    //getting brand id on the basis of brand title
+    $select_new_brand_id = "SELECT * FROM brands WHERE brand_title='$update_brand'";
+    $result_new_brand_id = mysqli_query($conn, $select_new_brand_id);
+    $row_new_brand_id = mysqli_fetch_assoc($result_new_brand_id);
+    
+    if ($row_new_brand_id) {
+        $new_brand_id = $row_new_brand_id['brand_id']; // Use square brackets
+    } else {
+        // Handle the case where the category was not found.
+        echo "<script>alert('problem')</script>";
+    }
+    
+
+    
+
+
+
+       // Check if a new image file has been uploaded for Image 1
+       if (empty($_FILES['product_image1']['name'])) {
+        // No new file for Image 1, keep the existing value
+        $product_image1 = $product_image1; // Replace with the actual value from the database
+    } else {
+        $product_image1 = $_FILES['product_image1']['name'];
+        $product_image_tmp1 = $_FILES['product_image1']['tmp_name'];
+        move_uploaded_file($product_image_tmp1, "./product_images/$product_image1");
+    }
+
+    if (empty($_FILES['product_image2']['name'])) {
+        $product_image2 = $product_image2; // No new file for Image 2, keep existing value
+    } else {
+        $product_image2 = $_FILES['product_image2']['name'];
+        $product_image_tmp2 = $_FILES['product_image2']['tmp_name'];
+        move_uploaded_file($product_image_tmp2, "./product_images/$product_image2");
+    }
+
+    if (empty($_FILES['product_image3']['name'])) {
+        $product_image3 = $product_image3; // No new file for Image 3, keep existing value
+    } else {
+        $product_image3 = $_FILES['product_image3']['name'];
+        $product_image_tmp3 = $_FILES['product_image3']['tmp_name'];
+        move_uploaded_file($product_image_tmp3, "./product_images/$product_image3");
+    }
+    
+        // The rest of your code for SQL query and form processing
+
+    //price
+    $update_price=$_POST['product_price'];
+
+    if($update_title=='' or $update_description=='' or $update_description=='' or $update_category=='' or $update_brand=='' or $product_image1=='' or $product_image2=='' or $product_image3=='' or $product_price=='')
+    {
+        echo "<script>alert('All fields are required!')</script>";
+            echo"<script>window.open('edit_products.php' ,'_self') </script>";
+    }
+    else{
+        $update_query = "UPDATE `products` SET 
+        `product_title`='$update_title',
+        `product_description`='$update_description',
+        `product_keywords`='$update_keyword',
+        `category_id`='$new_cat_id',
+        `brand_id`=' $new_brand_id',
+        `product_image1`='$product_image1',
+        `product_image2`='$product_image2',
+        `product_image3`='$product_image3',
+        `product_price`='$update_price',
+        `date`=NOW() 
+        WHERE product_id=$product_id";
+    
+    $result_update=mysqli_query($conn,$update_query);
+
+    if($result_update)
+    {
+        echo "<script>alert('updated successfully')</script>";
+        echo"<script>window.open('insert_product.php' ,'_self') </script>";
+
+    }
+    else{
+        echo "<script>alert('Not updated ')</script>";
+    }
+
+
+    }
+
+}
  ?>
 
 <!DOCTYPE html>
@@ -66,10 +176,10 @@ if(isset($_GET['edit_products']))
 
   <div class="container">
    
-    <form action="" method="post">
+    <form action="" method="post"  enctype="multipart/form-data">
         <div class="form-outline mb-4 ">
             <label for="product_title" >Product Title</label>
-            <input type="text" name="" id="product_title" class="form-control w-75 " value="<?php echo $product_title ?>" required="required">
+            <input type="text" name="product_title" id="product_title" class="form-control w-75 " value="<?php echo $product_title ?>" required="required">
         </div>
 
         <div class="form-group  mb-3">
@@ -79,7 +189,7 @@ if(isset($_GET['edit_products']))
 
         <div class="form-group mb-3">
             <label for="product_keyword">Product Keyword</label>
-            <input type="text" name="" id="product_keyword" class="form-control w-75" value="<?php echo $product_keywords ?>" required="required">
+            <input type="text" name="product_keyword"  class="form-control w-75" value="<?php echo $product_keywords ?>" required="required">
         </div>
 
         <div class="form-group mb-3">
@@ -131,7 +241,7 @@ if(isset($_GET['edit_products']))
         </div>
 
         <div class="from-group d-flex align-items-start ">
-            <input type="file" name="product_image1" id="" class="form-control w-75 ">
+            <input type="file" name="product_image1" class="form-control w-75 ">
             <img src="./product_images/<?php echo $product_image1 ?>"  class="edit_image">
 
         </div>
@@ -141,7 +251,7 @@ if(isset($_GET['edit_products']))
         </div>
 
         <div class="from-group d-flex align-items-start">
-            <input type="file" name="" id="product_image2" class="form-control w-75">
+            <input type="file" name="product_image2"  class="form-control w-75">
             <img src="./product_images/<?php echo $product_image2 ?>"  class="edit_image">
 
         </div>
@@ -151,14 +261,14 @@ if(isset($_GET['edit_products']))
         </div>
 
         <div class="from-group  d-flex align-items-start">
-            <input type="file" name="product_image3" id="" class="form-control w-75">
+            <input type="file" name="product_image3" class="form-control w-75">
             <img src="./product_images/<?php echo $product_image3 ?>"  class="edit_image">
 
         </div>
 
         <div class="form-group mb-3">
             <label for="product_price">Product Price</label>
-            <input type="text" name="product_price" id="" class="form-control w-75" value="<?php echo $product_price ?>" required="required">
+            <input type="text" name="product_price"  class="form-control w-75" value="<?php echo $product_price ?>" required="required">
         </div>
 
         <div class="form-group mb-3 ">
